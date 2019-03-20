46e97941738872955a4a2cc81907edd0207c08e3
Oliver Earl <ole4@aber.ac.uk>
2019-03-20 20:57:00 +0000

    Okay, database connectivity is done

--- public_html/index.php
+++ public_html/index.php
@@ -1,7 +1,10 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 require '../vendor/autoload.php';
 
-$app = csm2020\PatientApp\PatientApp::init();
+$app = \csm2020\PatientApp\PatientApp::init();
 
 $app->run();
