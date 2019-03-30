5bde26d336d45d9c45692e6ed2afac00d58fae1c
Oliver Earl <ole4@aber.ac.uk>
2019-03-30 00:40:49 +0000

    Refactoring for User model

---
+++ src/Controllers/UserController.php
@@ -0,0 +1,12 @@
+<?php
+namespace csm2020\PatientApp\Controllers;
+
+use csm2020\PatientApp\Database\Database;
+
+use PDO;
+use PDOException;
+
+class UserController
+{
+
+}
---
+++ src/Models/User.php
@@ -0,0 +1,136 @@
+<?php
+namespace csm2020\PatientApp\Models;
+
+class User
+{
+    private $userId;
+    private $username;
+    private $userPassword;
+    private $userEmail;
+    private $userFirstName;
+
+    /**
+     * @return mixed
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @param mixed $userId
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param mixed $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserPassword()
+    {
+        return $this->userPassword;
+    }
+
+    /**
+     * @param mixed $userPassword
+     */
+    public function setUserPassword($userPassword)
+    {
+        $this->userPassword = $userPassword;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserEmail()
+    {
+        return $this->userEmail;
+    }
+
+    /**
+     * @param mixed $userEmail
+     */
+    public function setUserEmail($userEmail)
+    {
+        $this->userEmail = $userEmail;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserFirstName()
+    {
+        return $this->userFirstName;
+    }
+
+    /**
+     * @param mixed $userFirstName
+     */
+    public function setUserFirstName($userFirstName)
+    {
+        $this->userFirstName = $userFirstName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserLastName()
+    {
+        return $this->userLastName;
+    }
+
+    /**
+     * @param mixed $userLastName
+     */
+    public function setUserLastName($userLastName)
+    {
+        $this->userLastName = $userLastName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserType()
+    {
+        return $this->userType;
+    }
+
+    /**
+     * @param mixed $userType
+     */
+    public function setUserType($userType)
+    {
+        $this->userType = $userType;
+    }
+    private $userLastName;
+    private $userType;
+
+    public function __construct($uid, $username, $password, $email, $firstName, $lastName, $userType)
+    {
+        $this->userId =         $uid;
+        $this->username =       $username;
+        $this->userPassword =   $password;
+        $this->userEmail =      $email;
+        $this->userFirstName =  $firstName;
+        $this->userLastName =   $lastName;
+        $this->userType =       $userType;
+    }
+}
