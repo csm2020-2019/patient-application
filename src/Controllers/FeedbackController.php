031adeaea4f82ef925624cdbb41366f4a5f853d8
Oliver Earl <ole4@aber.ac.uk>
2019-04-01 05:02:19 +0100

    Feedback doc

--- src/Controllers/FeedbackController.php
+++ src/Controllers/FeedbackController.php
@@ -4,6 +4,11 @@ namespace csm2020\PatientApp\Controllers;
 use csm2020\PatientApp\Config\Config;
 use csm2020\PatientApp\Database\Database;
 
+/**
+ * Class FeedbackController
+ * @package csm2020\PatientApp\Controllers
+ * @author Oliver Earl <ole4@aber.ac.uk>
+ */
 class FeedbackController
 {
     const ANONYMOUS = 'noreply@feedback.com';
@@ -15,6 +20,23 @@ class FeedbackController
         $this->recipient = Config::getConfig()['email-recipient'] ?? self::ANONYMOUS;
     }
 
+    /**
+     * Feedback Method
+     * @param $content
+     * @param string $from
+     * @return bool|null
+     *
+     * This method takes two parameters - one is the feedback content, and the other is an email address, which is
+     * optional should the user desire an email response.
+     *
+     * After doing some initial sanitisation of inputs, returning null should any of them fail, it builds an email
+     * message with the gathered information and sends to to the email address stored in the program's configuration. If
+     * this succeeds, and the user opted to provide an email address and isn't an anonymous submitter, the method will
+     * also send a secondary email confirming and thanking the user for their feedback.
+     *
+     * If it all goes right, true is returned by the method. If any problems are ran into by PHP's mail methods then
+     * null will also be returned, causing an error message on the frontend.
+     */
     public function feedback($content, $from = self::ANONYMOUS)
     {
         $content =  Database::sanitise($content);
