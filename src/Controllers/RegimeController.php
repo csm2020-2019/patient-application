371a7e568a4306cef3ab74c10a3f2ee57d0c03e2
Oliver Earl <ole4@aber.ac.uk>
2019-04-01 05:16:20 +0100

    PHPDoc 2 Electric Boogaloo

--- src/Controllers/RegimeController.php
+++ src/Controllers/RegimeController.php
@@ -5,12 +5,27 @@ use csm2020\PatientApp\Models\Patient;
 use csm2020\PatientApp\Models\Regime;
 use csm2020\PatientApp\Models\Trial;
 
-
+/**
+ * Class RegimeController
+ * @package csm2020\PatientApp\Controllers
+ * @author Oliver Earl <ole4@aber.ac.uk>
+ */
 class RegimeController
 {
 
     public function __construct() {}
 
+    /**
+     * Regimes Method
+     * @param $uid
+     * @return array|null
+     *
+     * Using a user's ID, this method first builds its patient equivalent by ID. If it exists, it will proceed to pull
+     * all regimes attached to that current patient and builds up an array. If there's none, this will still return
+     * an empty array which will be interpreted by the frontend as simply not having any regimes. Otherwise, a patient
+     * can have limitless regimes set by their GP. If anything goes wrong, this method returns null, which is
+     * interpreted as an error by the rest of the application.
+     */
     public function regimes($uid)
     {
         $patient = Patient::getPatientByUserId($uid);
@@ -25,6 +40,19 @@ class RegimeController
         return null;
     }
 
+    /**
+     * Regime Method
+     * @param $rid
+     * @param $uid
+     * @return array|null
+     *
+     * Using a specific regime ID and user ID, this method returns all the details needed for the specific regime view.
+     * This includes not only the regime information contained in the regime object, but also all of the trials that
+     * are associated to the specific regime by invoking a Trials Controller object, as well as gathering all the
+     * contact information of the GP who assigned the regime from a method in the User Controller.
+     *
+     * Returns a large associative array of information for use by the frontend, or null.
+     */
     public function regime($rid, $uid)
     {
         $patient = Patient::getPatientByUserId($uid);
