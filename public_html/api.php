5b1f0feb7f9003a0f98d87df5db69aed92a25be6
Oliver Earl <ole4@aber.ac.uk>
2019-04-01 06:22:16 +0100

    Easter egg

--- public_html/api.php
+++ public_html/api.php
@@ -1,6 +1,11 @@
 <?php
 require '../vendor/autoload.php';
 
+// Hey!
+// Did you know?
+// The final release of me was written in 13 days. Documentation, tests, and all.
+// Not bad.
+
 $app = \csm2020\PatientApp\PatientApp::init();
 
 $app->run();
