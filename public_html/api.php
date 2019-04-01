c80093b229537d3c38dec8111f1dbfdfbeba1225
Oliver Earl <ole4@aber.ac.uk>
2019-04-01 03:34:36 +0100

    Removed debugging commands

--- public_html/api.php
+++ public_html/api.php
@@ -1,9 +1,4 @@
 <?php
-//declare(strict_types = 1);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 require '../vendor/autoload.php';
 
 $app = \csm2020\PatientApp\PatientApp::init();
