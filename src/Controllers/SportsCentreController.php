b8add1712c01aa67bd4c6a073444e07d57782e67
Oliver Earl <ole4@aber.ac.uk>
2019-04-01 05:49:46 +0100

    The trilogy of PHPDoc

--- src/Controllers/SportsCentreController.php
+++ src/Controllers/SportsCentreController.php
@@ -4,14 +4,32 @@ namespace csm2020\PatientApp\Controllers;
 use csm2020\PatientApp\Models\Patient;
 use csm2020\PatientApp\Models\SportsCentre;
 
+/**
+ * Class SportsCentreController
+ * @package csm2020\PatientApp\Controllers
+ * @author Oliver Earl <ole4@aber.ac.uk>
+ */
 class SportsCentreController
 {
     public function __construct() {}
 
+    /**
+     * Get All Sports Centres Method
+     * @return array|null
+     *
+     * This method will attempt to retrieve all the sports centres stored in the database. If there aren't any, for
+     * some bizarre reason, then an empty array is returned and the frontend is free to interpret that however it wishes,
+     * which is at this time of writing a simple notice that none are on the system currently, which could well be the
+     * case somehow. Otherwise, an array of all the sports centres and their respective information is returned for
+     * rendering. If something somewhere goes wrong, null is returned.
+     */
     public function getAllSportsCentres()
     {
         $centres = [];
         $results = SportsCentre::getAllSportCentres();
+        if ($results === null) {
+            return null;
+        }
         if ($results !== []) {
             foreach ($results as $result) {
                 array_push($centres, $result->toAssoc());
@@ -20,6 +38,20 @@ class SportsCentreController
         return $centres;
     }
 
+    /**
+     * Set Sports Centre
+     * @param $uid
+     * @param $scid
+     * @return bool|null
+     *
+     * This method is used to assign a user to their preferred sports centre - sometimes referred to as an assignment
+     * or a preference elsewhere in the program. It takes the current user ID, and the sports centre ID provided by the
+     * route from the frontend.
+     *
+     * All 'appointments' associated with the patient are first removed with prejudice, just in case. Then from there,
+     * the patient has their appointment set. Database or sanitisation errors will cause this method to return null,
+     * or true should everything go smoothly.
+     */
     public function setSportsCentre($uid, $scid)
     {
         $patient = Patient::getPatientByUserId($uid);
--- src/Database/Database.php
+++ src/Database/Database.php
@@ -6,6 +6,11 @@ use csm2020\PatientApp\Config\Config;
 use PDO;
 use PDOException;
 
+/**
+ * Class Database
+ * @package csm2020\PatientApp\Database
+ * @author Oliver Earl <ole4@aber.ac.uk>
+ */
 class Database
 {
     private static $db;
@@ -13,6 +18,15 @@ class Database
     private function __construct() {}
     private function __clone() {}
 
+    /**
+     * Get Database Method
+     * @return PDO
+     *
+     * Arguably the most important method in the entire program. This static method will return a PDO singleton to
+     * whatever calls it, allowing for database connectivity, predominantly in models.
+     *
+     * Should the database fail at the initialisation stage, the program will fall over.
+     */
     public static function getDatabase(): PDO
     {
         if (!isset(self::$db)) {
@@ -23,13 +37,23 @@ class Database
             } catch (PDOException $ex) {
                 // TODO: Better error handling
                 echo 'Database error has occurred.';
-                echo $ex->getMessage();
                 die();
             }
         }
         return self::$db;
     }
 
+    /**
+     * Sanitise Helper Method
+     * @param $input
+     * @return string
+     *
+     * Static method that can be called wherever the Database class is being used (a lot of places!) that will clean
+     * a string of any naughty business, including HTML tags, Unicode characters, and whitespace. It's not completely
+     * foolproof, especially against SQL injection, which is why it's important to use it in conjunction with using
+     * prepared statements, as all uses of the PDO object do as of this time of writing. It will however defuse any
+     * JavaScript injection attacks. Hopefully.
+     */
     public static function sanitise($input)
     {
         return trim(stripslashes(htmlspecialchars(strip_tags($input))));
